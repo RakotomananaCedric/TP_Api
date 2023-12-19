fa7ad06fd5673d7a72845ff2ca210873a1fb747e
RakotomananaCedric <cedric.rakotomanana2511@gmail.com>
2023-12-19 15:07:48 +0100

    API région + département + commune 19/12/2023

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -3,16 +3,80 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class ApiController extends AbstractController
 {
-    #[Route('/api', name: 'app_api')]
-    public function index(): Response
+    /**
+     * @Route("liste_des_régions", name="listeRegions")
+     */
+    public function listeRegion(SerializerInterface $serializer)
     {
-        return $this->render('api/index.html.twig', [
-            'controller_name' => 'ApiController',
+        $listeRegions=file_get_contents('https://geo.api.gouv.fr/regions');
+        // $tableauRegion=$serializer->decode($listeRegions,'json');
+        // $objetRegion=$serializer->denormalize($tableauRegion,'App\Entity\Region[]');
+        $objetRegion=$serializer->deserialize($listeRegions,'App\Entity\Region[]','json');
+        return $this->render('api/listeRegions.html.twig',[
+            'listeRegions'=>$objetRegion
+        ]);
+    }
+
+    /**
+     * @Route("liste_des_départements_par_région", name="listeDepartements")
+     */
+    public function listeDepartementParRegion(SerializerInterface $serializer, Request $request)
+    {
+        $codeRegion=$request->query->get('region');
+        $listeRegions=file_get_contents('https://geo.api.gouv.fr/regions');
+        $objetRegion=$serializer->deserialize($listeRegions,'App\Entity\Region[]','json');
+        if($codeRegion == null || $codeRegion == "Toutes")
+        {
+            $listeDepartements=file_get_contents('https://geo.api.gouv.fr/departements');
+        }
+        else
+        {
+            $listeDepartements=file_get_contents('https://geo.api.gouv.fr/regions/'.$codeRegion.'/departements');
+        }
+        $listeDepartements=$serializer->decode($listeDepartements,'json');
+        return $this->render('api/listeDepartParRegion.html.twig',[
+            'listeRegions'=>$objetRegion,
+            'listeDepartements'=>$listeDepartements
+        ]);
+    }
+
+    /**
+     * @Route("liste_des_départements_par_région", name="listeDepartements")
+     */
+    public function listeCommuneParDepartement(SerializerInterface $serializer, Request $request)
+    {
+        $codeRegion=$request->query->get('region');
+        $codeDepartement=$request->query->get('departement');
+        $listeRegions=file_get_contents('https://geo.api.gouv.fr/regions');
+        $objetRegion=$serializer->deserialize($listeRegions,'App\Entity\Region[]','json');
+        if($codeRegion == null || $codeRegion == "Toutes")
+        {
+            $listeDepartements=file_get_contents('https://geo.api.gouv.fr/departements');
+        }
+        else
+        {
+            $listeDepartements=file_get_contents('https://geo.api.gouv.fr/regions/'.$codeRegion.'/departements');
+        }
+        if($codeDepartement == null || $codeDepartement == "Toutes")
+        {
+            $listeCommunes=file_get_contents('https://geo.api.gouv.fr/commune');
+        }
+        else
+        {
+            $listeCommunes=file_get_contents('https://geo.api.gouv.fr/departement/'.$codeDepartement.'/commune');
+        }
+        $listeDepartements=$serializer->decode($listeDepartements,'json');
+        $listeCommunes=$serializer->decode($listeCommunes,'json');
+        return $this->render('api/listeCommuneParDepart.html.twig',[
+            'listeRegions'=>$objetRegion,
+            'listeDepartements'=>$listeDepartements,
+            'listeCommune'=>$listeCommunes
         ]);
     }
 }
